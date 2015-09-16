50672a29c44cc8762d7ff988c7d539f982d43874
xc-racer99 <xc-racer2@live.ca>
2015-09-16 09:13:35 -0700

    Revert "fascinate: Don't copy RIL blobs"
    We're trying again!
    This reverts commit a767b180378166ba7c27a54c5342cac4cc2d4c6a.

--- fascinatemtd/fascinatemtd-vendor-blobs.mk
+++ fascinatemtd/fascinatemtd-vendor-blobs.mk
@@ -13,22 +13,21 @@
 # limitations under the License.
 
 # Prebuilt libraries that are needed to build open-source libraries
-#PRODUCT_COPY_FILES := \
-#    vendor/samsung/fascinatemtd/proprietary/libril.so:obj/lib/libril.so \
-#    vendor/samsung/fascinatemtd/proprietary/libsecril-client.so:obj/lib/libsecril-client.so
+PRODUCT_COPY_FILES := \
+    vendor/samsung/fascinatemtd/proprietary/libril.so:obj/lib/libril.so \
+    vendor/samsung/fascinatemtd/proprietary/libsecril-client.so:obj/lib/libsecril-client.so
 
 
 # All the blobs necessary for fascinatemtd devices
 PRODUCT_COPY_FILES += \
+    vendor/samsung/fascinatemtd/proprietary/libril.so:system/lib/libril.so \
+    vendor/samsung/fascinatemtd/proprietary/libsecril-client.so:system/lib/libsecril-client.so \
+    vendor/samsung/fascinatemtd/proprietary/libsec-ril40.so:system/lib/libsec-ril40.so \
+    vendor/samsung/fascinatemtd/proprietary/rild:system/bin/rild \
+    vendor/samsung/fascinatemtd/proprietary/pppd_runner:system/bin/pppd_runner \
     vendor/samsung/fascinatemtd/proprietary/gpsd:system/vendor/bin/gpsd \
     vendor/samsung/fascinatemtd/proprietary/gps.conf:system/etc/gps.conf \
     vendor/samsung/fascinatemtd/proprietary/gps.aries.so:system/lib/hw/gps.aries.so \
     vendor/samsung/fascinatemtd/proprietary/libsensor_yamaha_test.so:system/vendor/lib/libsensor_yamaha_test.so \
     vendor/samsung/fascinatemtd/proprietary/geomagneticd:system/vendor/bin/geomagneticd \
     vendor/samsung/fascinatemtd/proprietary/orientationd:system/vendor/bin/orientationd
-
-#    vendor/samsung/fascinatemtd/proprietary/libril.so:system/lib/libril.so \
-#    vendor/samsung/fascinatemtd/proprietary/libsecril-client.so:system/lib/libsecril-client.so \
-#    vendor/samsung/fascinatemtd/proprietary/libsec-ril40.so:system/lib/libsec-ril40.so \
-#    vendor/samsung/fascinatemtd/proprietary/rild:system/bin/rild \
-#    vendor/samsung/fascinatemtd/proprietary/pppd_runner:system/bin/pppd_runner \
