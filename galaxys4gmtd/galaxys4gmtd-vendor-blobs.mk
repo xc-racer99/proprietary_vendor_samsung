8d0d1b831e70e3a2d6e6fbaa6c342795f37b73a2
xc-racer99 <xc-racer2@live.ca>
2017-12-04 12:54:24 -0800

    galaxys4g: Replace tabs with spaces

--- galaxys4gmtd/galaxys4gmtd-vendor-blobs.mk
+++ galaxys4gmtd/galaxys4gmtd-vendor-blobs.mk
@@ -17,24 +17,24 @@
 PRODUCT_COPY_FILES += \
     vendor/samsung/aries-common/proprietary/bcm4329.hcd:system/vendor/firmware/bcm4329.hcd \
     vendor/samsung/aries-common/proprietary/samsung_mfc_fw.bin:system/vendor/firmware/samsung_mfc_fw.bin \
-	vendor/samsung/galaxys4gmtd/proprietary/Certification.cer:system/vendor/bin/Certification.cer \
-	vendor/samsung/galaxys4gmtd/proprietary/gps.aries.so:system/vendor/lib/hw/gps.aries.so \
-	vendor/samsung/galaxys4gmtd/proprietary/gps.conf:system/etc/gps.conf \
-	vendor/samsung/galaxys4gmtd/proprietary/gps.xml:system/etc/gps.xml \
-	vendor/samsung/galaxys4gmtd/proprietary/gps.xml:system/vendor/etc/gps.xml \
-	vendor/samsung/galaxys4gmtd/proprietary/libclientgps.so:system/lib/libclientgps.so \
-	vendor/samsung/galaxys4gmtd/proprietary/libsec-ril.so:system/lib/libsec-ril.so \
-	vendor/samsung/galaxys4gmtd/proprietary/modem.bin:modem.bin \
-	vendor/samsung/galaxys4gmtd/proprietary/nvram_net.txt:system/vendor/firmware/nvram_net.txt \
-	vendor/samsung/galaxys4gmtd/proprietary/samsung-gpsd:system/vendor/bin/samsung-gpsd \
-	vendor/samsung/galaxys4gmtd/proprietary/secgps.conf:system/etc/vendor/samsung-agps/secgps.conf \
-	vendor/samsung/galaxys4gmtd/proprietary/telusgalaxys4gmtd/agpsd:/system/vendor/bin/agpsd \
-	vendor/samsung/galaxys4gmtd/proprietary/telusgalaxys4gmtd/gps.aries.so.telusgalaxys4gmtd:system/vendor/lib/hw/gps.aries.so.telusgalaxys4gmtd \
-	vendor/samsung/galaxys4gmtd/proprietary/telusgalaxys4gmtd/libclientgps.so.telusgalaxys4gmtd:system/lib/libclientgps.so.telusgalaxys4gmtd \
-	vendor/samsung/galaxys4gmtd/proprietary/telusgalaxys4gmtd/modem.bin.telusgalaxys4gmtd:modem.bin.telusgalaxys4gmtd \
-	vendor/samsung/galaxys4gmtd/proprietary/telusgalaxys4gmtd/ste-cg29xx_ctrl:system/vendor/bin/ste-cg29xx_ctrl \
-	vendor/samsung/aries-common/proprietary/geomagneticd:system/vendor/bin/geomagneticd \
-	vendor/samsung/aries-common/proprietary/orientationd:system/vendor/bin/orientationd
+    vendor/samsung/galaxys4gmtd/proprietary/Certification.cer:system/vendor/bin/Certification.cer \
+    vendor/samsung/galaxys4gmtd/proprietary/gps.aries.so:system/vendor/lib/hw/gps.aries.so \
+    vendor/samsung/galaxys4gmtd/proprietary/gps.conf:system/etc/gps.conf \
+    vendor/samsung/galaxys4gmtd/proprietary/gps.xml:system/etc/gps.xml \
+    vendor/samsung/galaxys4gmtd/proprietary/gps.xml:system/vendor/etc/gps.xml \
+    vendor/samsung/galaxys4gmtd/proprietary/libclientgps.so:system/lib/libclientgps.so \
+    vendor/samsung/galaxys4gmtd/proprietary/libsec-ril.so:system/lib/libsec-ril.so \
+    vendor/samsung/galaxys4gmtd/proprietary/modem.bin:modem.bin \
+    vendor/samsung/galaxys4gmtd/proprietary/nvram_net.txt:system/vendor/firmware/nvram_net.txt \
+    vendor/samsung/galaxys4gmtd/proprietary/samsung-gpsd:system/vendor/bin/samsung-gpsd \
+    vendor/samsung/galaxys4gmtd/proprietary/secgps.conf:system/etc/vendor/samsung-agps/secgps.conf \
+    vendor/samsung/galaxys4gmtd/proprietary/telusgalaxys4gmtd/agpsd:/system/vendor/bin/agpsd \
+    vendor/samsung/galaxys4gmtd/proprietary/telusgalaxys4gmtd/gps.aries.so.telusgalaxys4gmtd:system/vendor/lib/hw/gps.aries.so.telusgalaxys4gmtd \
+    vendor/samsung/galaxys4gmtd/proprietary/telusgalaxys4gmtd/libclientgps.so.telusgalaxys4gmtd:system/lib/libclientgps.so.telusgalaxys4gmtd \
+    vendor/samsung/galaxys4gmtd/proprietary/telusgalaxys4gmtd/modem.bin.telusgalaxys4gmtd:modem.bin.telusgalaxys4gmtd \
+    vendor/samsung/galaxys4gmtd/proprietary/telusgalaxys4gmtd/ste-cg29xx_ctrl:system/vendor/bin/ste-cg29xx_ctrl \
+    vendor/samsung/aries-common/proprietary/geomagneticd:system/vendor/bin/geomagneticd \
+    vendor/samsung/aries-common/proprietary/orientationd:system/vendor/bin/orientationd
 
 PRODUCT_PACKAGES += \
     libGLESv1_CM_POWERVR_SGX540_120 \
